5f54891f06d5b4b8609ecd5da5f9169c998ac291
GaryQian <garyq@google.com>
2019-06-26 18:23:35 -0700

    cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -63,11 +63,11 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return new Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      // appBar: AppBar(
+      //   // Here we take the value from the MyHomePage object that was created by
+      //   // the App.build method, and use it to set our appbar title.
+      //   title: Text(widget.title),
+      // ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
@@ -93,43 +93,11 @@ class _MyHomePageState extends State<MyHomePage> {
                   right: 0,
                   bottom: 0,
                   child: Transform.translate(
-                  // Align(
                     offset: Offset(5, 5),
-                    // alignment: Alignment(1, 1),
-                    // left: 260,
-                    // top: 550,
-                    // width: 100,
-                    // height: 100,
-                    // right: 20,
-                    // bottom: 0,
-                    // child: Container(
-                    //   width: 100,
-                    //   height: 100,
-                    //   decoration: BoxDecoration(
-                    //     color: Colors.blue,
-                    //     border: Border.all(
-                    //       color: Colors.blue,
-                    //       width: 14.0,
-                    //     ),
-                    //   ),
-                    // ), // Container
                     child: CustomPaint(
                       painter: ShapesPainter(),
                       child: Container(height: 50, width: 50),
                     ),
-                    // child: RotationTransition(
-                    //   turns: AlwaysStoppedAnimation(90 / 360),
-                    //   child: Material(
-                    //     borderRadius: BorderRadius.all(
-                    //       Radius.circular(1.5),
-                    //     ),
-                    //     color: Colors.blue,
-                    //     child: Container(
-                    //       height: 10.0,
-                    //       width: 10.0,
-                    //     ),
-                    //   ),
-                    // ),
                   ),
                 ),
                 ClipRRect(
@@ -177,7 +145,7 @@ class _MyHomePageState extends State<MyHomePage> {
               ],
             ), // Stack
             Text(
-              '$_counter',
+              '',
               style: Theme.of(context).textTheme.display1,
             ),
           ],
