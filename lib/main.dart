1ef883fdf91ee5b326efcb3236c9f7de11cb5bbf
Chris Yang <ychris@google.com>
2019-07-29 17:26:01 -0700

    revert the response reading

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,9 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'gallery/app.dart';
 
+const String emoji1 = "assets/devil.gif";
+const String emoji2 = "assets/tears.gif";
+
 void main() {
   runApp(MaterialApp(
     theme: ThemeData(
@@ -29,181 +32,117 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: FutureBuilder(
-          future: getResponse('responses/response.json'),
-          builder:
-              (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
-            if (!snapshot.hasData) {
-              return Container();
-            }
-            return Center(
-              child: ListView.separated(
-                itemCount: snapshot.data.length,
-                padding: const EdgeInsets.only(left: 20, right: 20, top: 100),
-                itemBuilder: (BuildContext context, int index) {
-                  Map<String, dynamic> bubbleData =
-                      snapshot.data[index] as Map<String, dynamic>;
-                  bool isLeft = bubbleData["isLeft"];
-                  return Row(children: [
-                    Spacer(flex: !isLeft ? 100 : 1),
-                    InteractiveBubble(
-                      isLeft: isLeft,
-                      data: bubbleData,
-                    ),
-                    Spacer(flex: isLeft ? 100 : 1),
-                  ]);
-                },
-                separatorBuilder: (BuildContext context, int index) =>
-                    SizedBox(height: 35),
-              ),
-            );
-          }),
-    );
-  }
-
-  Future<List<dynamic>> getResponse(String assetName) async {
-    String data = await DefaultAssetBundle.of(context).loadString(assetName);
-    return jsonDecode(data);
+        body: Center(
+            child: ListView.separated(
+      itemCount: 1,
+      padding: const EdgeInsets.only(left: 20, right: 20, top: 100),
+      itemBuilder: (BuildContext context, int index) {
+        return Row(children: [
+          Spacer(flex: 100),
+          Bubble(),
+          Spacer(flex: 1),
+        ]);
+      },
+      separatorBuilder: (BuildContext context, int index) =>
+          SizedBox(height: 35),
+    )));
   }
 }
 
-class InteractiveBubble extends StatefulWidget {
-  InteractiveBubble({
-    this.isLeft,
-    this.data,
-  });
-
-  final double minScale = 0.5;
-  final double maxScale = 2.5;
-
-  final Map<String, dynamic> data;
-
-  final bool isLeft;
-
+class Bubble extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => InteractiveBubbleState();
+  _BubbleState createState() => _BubbleState();
 }
 
-class InteractiveBubbleState extends State<InteractiveBubble>
-    with SingleTickerProviderStateMixin {
-  InteractiveBubbleState();
-
+class _BubbleState extends State<Bubble> {
+  final double _fontSize = 20;
   double _fontScale = 1;
   double _startScale = 1;
-
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onScaleStart: (scaleDetails) => setState(() => _startScale = _fontScale),
-      onScaleUpdate: (ScaleUpdateDetails scaleDetails) {
-        setState(() {
-          _fontScale = (scaleDetails.scale * _startScale)
-              .clamp(widget.minScale, widget.maxScale);
-        });
-      },
-      child: BubbleBuilder().buildBubbleWithData(widget.data, _fontScale),
-    );
-  }
-}
-
-class BubbleBuilder {
-  Bubble buildBubbleWithData(Map<String, dynamic> data, double scale) {
-    bool isLeft = data['isLeft'];
-    double strutFontSize = data['strutFontSize'];
-    List<InlineSpan> spans = [];
-    List<dynamic> spansData = data['spans'];
-    for (Map<String, dynamic> spanData in spansData) {
-      String type = spanData['type'];
-      switch (type) {
-        case 'text':
-          spans.add(TextSpan(
-              text: spanData['text'],
-              style: TextStyle(
-                  fontSize: spanData['fontSize'] * scale, color: Colors.black)));
-          break;
-        case 'image':
-          spans.add(WidgetSpan(
-              child: Image.asset(spanData['asset'],
-                  width: spanData['width'] * scale, height: spanData['height'] * scale)));
-          break;
-      }
-    }
-    Text text = Text.rich(
-      TextSpan(children: spans),
-      strutStyle: StrutStyle(fontSize: strutFontSize, height: 1.1),
-    );
-    return Bubble(isLeft: isLeft, text: text);
-  }
-}
-
-class Bubble extends StatelessWidget {
-  Bubble({
-    this.isLeft,
-    this.text,
-  });
-
-  final double padding = 15;
-  final double radius = 15;
-
-  final bool isLeft;
-  final Text text;
+  final double _minScale = 0.5;
+  final double _maxScale = 2.5;
 
   @override
   Widget build(BuildContext context) {
-    List<Color> gradientColors = isLeft
-        ? [Colors.grey[200], Colors.grey[350]]
-        : [Colors.lightGreenAccent[700], Colors.green[600]];
-    return CustomPaint(
-      painter: BubbleShadowPainter(isLeft: isLeft, radius: radius),
-      child: ClipRRect(
-        borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(radius),
-            topRight: Radius.circular(radius),
-            bottomLeft: isLeft ? Radius.zero : Radius.circular(radius),
-            bottomRight: isLeft ? Radius.circular(radius) : Radius.zero),
-        child: Container(
-          constraints: BoxConstraints(
-            maxWidth: MediaQuery.of(context).size.width * 0.7,
-          ),
-          padding: EdgeInsets.all(padding),
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: gradientColors,
-              begin: Alignment.topLeft,
-              end: Alignment.bottomLeft,
+    return GestureDetector(
+      onScaleStart: (ScaleStartDetails scaleDetails) {
+        setState(() => _startScale = _fontScale);
+      },
+      onScaleUpdate: (ScaleUpdateDetails scaleUpdateDetails) {
+        setState(() => _fontScale = (scaleUpdateDetails.scale * _startScale)
+            .clamp(_minScale, _maxScale));
+      },
+      child: CustomPaint(
+        painter: BubbleShadowPainter(),
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(15),
+              topRight: Radius.circular(15),
+              bottomLeft: Radius.circular(15)),
+          child: Container(
+            constraints: BoxConstraints(maxWidth: 400),
+            padding: EdgeInsets.all(15),
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Colors.lightGreenAccent[700], Colors.green[600]],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomLeft)),
+            child: Text.rich(
+              buildTextSpan(),
+              strutStyle: StrutStyle(fontSize: _fontSize * _fontScale),
             ),
           ),
-          child: text,
         ),
       ),
     );
   }
+
+  TextSpan buildTextSpan() {
+    return TextSpan(
+        style: TextStyle(
+            fontSize: _fontSize * _fontScale,
+            decoration: TextDecoration.underline,
+            decorationStyle: TextDecorationStyle.wavy,
+            decorationColor: Colors.red),
+        children: <InlineSpan>[
+          TextSpan(text: 'Hello everyone.\n'),
+          WidgetSpan(
+              child: Image.asset(
+            emoji1,
+            width: 25 * _fontScale,
+            height: 25 * _fontScale,
+          )),
+          WidgetSpan(
+              child: Image.asset(
+            emoji2,
+            width: 25 * _fontScale,
+            height: 25 * _fontScale,
+          )),
+          TextSpan(
+              text: 'The small text\n',
+              style: TextStyle(fontSize: _fontSize / 2 * _fontScale)),
+          WidgetSpan(
+              child: SizedBox(width: 300, height: 400, child: GalleryApp())),
+        ]);
+  }
 }
 
 class BubbleShadowPainter extends CustomPainter {
-  BubbleShadowPainter({this.isLeft, this.radius});
-
-  final double radius;
-  final bool isLeft;
-
   @override
   void paint(Canvas canvas, Size size) {
-    canvas.drawShadow(Path()..addRRect(buildRRect(size, radius, isLeft)),
-        Colors.black87, 5.0, false);
+    canvas.drawShadow(
+        Path()
+          ..addRRect(RRect.fromRectAndCorners(
+              Rect.fromLTWH(0, 0, size.width, size.height),
+              topLeft: Radius.circular(15),
+              topRight: Radius.circular(15),
+              bottomLeft: Radius.circular(15))),
+        Colors.black87,
+        5.0,
+        false);
   }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
     return true;
-  }
-
-  RRect buildRRect(Size size, double radius, bool isLeft) {
-    Radius bottomLeft = isLeft ? Radius.zero : Radius.circular(radius);
-    Radius bottomRight = isLeft ? Radius.circular(radius) : Radius.zero;
-    return RRect.fromRectAndCorners(
-        Rect.fromLTRB(0, 0, size.width, size.height),
-        topLeft: Radius.circular(radius),
-        topRight: Radius.circular(radius),
-        bottomLeft: bottomLeft,
-        bottomRight: bottomRight);
   }
 }
